94447256e77092c5e2329ff9d1f27bd189a5d346
lena hunanian <lenasss20053112@gmail.com>
2025-05-14 12:23:30 +0400

    first commit

---
+++ helloWorld/helloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloWorldApp.swift
+//  helloWorld
+//
+//  Created by Lena Hunanian on 14.05.25.
+//
+
+import SwiftUI
+
+@main
+struct helloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
